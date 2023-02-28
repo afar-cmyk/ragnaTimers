772b2fb6a53e411151276e4c10d7288148e31524
Andrés F. Alvarez <andresfelipe.alvarez03@gmail.com>
2023-02-27 23:50:39 -0500

    Añadir Boilerplate de Electron y React

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h2>Hola mundo desde React en App.jsx</h2>
+  </div>,
+  document.getElementById("root")
+);
