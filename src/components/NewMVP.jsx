c46b0e27b95ccd5a30d06a5ab238a9f7eaf12cb9
Andrés F. Alvarez <andresfelipe.alvarez03@gmail.com>
2023-12-05 19:05:50 -0500

    Add basic stules to NewMVP component

--- src/components/NewMVP.jsx
+++ src/components/NewMVP.jsx
@@ -4,33 +4,50 @@ const NewMVP = ({ time }) => {
     console.log("Hola mundo desde Timear")
 
   return (
-    <>
+    
+    <div className="newMvp_container">
+      <div style={{display: 'flex', flexDirection: 'column', rowGap: 24}} >
+        <span className="newMvp_span_mvps">
+          <select name="MVP" id="nombre_mvp">
+            <option value="none" selected disabled hidden>Seleccione un MVP</option> 
+            <option value="Garm">Garm</option>
+            <option value="Atroce">Atroce</option>
+            <option value="Maya">Maya</option>
+            <option value="Ungoliant">Ungoliant</option>
+          </select>
 
-      <select name="MVP" id="nombre_mvp">
-        <option value="none" selected disabled hidden>Seleccione un MVP</option> 
-        <option value="Garm">Garm</option>
-        <option value="Atroce">Atroce</option>
-        <option value="Maya">Maya</option>
-        <option value="Ungoliant">Ungoliant</option>
-      </select>
+          <select name="MVP" id="nombre_mvp">
+            <option value="none" selected disabled hidden>Seleccione un Mapa</option> 
+            <option value="mapa1">mapa1</option>
+            <option value="mapa1">mapa1</option>
+          </select>
+        </span>
 
-      <select name="MVP" id="nombre_mvp">
-        <option value="none" selected disabled hidden>Seleccione un Mapa</option> 
-        <option value="mapa1">mapa1</option>
-        <option value="mapa1">mapa1</option>
-      </select>
+        <span className="newMvp_span_hora">
+          <button>a</button>
 
-      <button>Hora Actual</button>
+          <input type="number" name="Hora" id="hora_input" placeholder="Ingresar Hora" style={{ marginRight: 8 }} />
+          <span style={{color: '#ABABAB', fontSize: 28, lineHeight: 0, marginBottom: 7}}>:</span>
+          <input type="number" name="Minutos" id="hora_input" placeholder="Ingresar Minutos" style={{ marginLeft: 8 }} />
 
-      <input type="number" name="Hora" id="hora_input" placeholder="Ingresar Hora" />
-      <input type="number" name="Minutos" id="hora_input" placeholder="Ingresar Minutos" />
+          <select name="periodo" id="periodo_tiempo" class="custom-select" className="select_periodo">
+            <option value="none" selected disabled hidden>Periodo</option> 
+            <option value="AM">AM</option>
+            <option value="PM">PM</option>
+          </select>
+
+        </span>
+      </div>
+
+
+
+
       
-      <select name="MVP" id="nombre_mvp">
-        <option value="none" selected disabled hidden>Periodo</option> 
-        <option value="AM">AM</option>
-        <option value="PM">PM</option>
-      </select>
-    </>
+
+      
+      
+      
+    </div>
   );
 };
 
