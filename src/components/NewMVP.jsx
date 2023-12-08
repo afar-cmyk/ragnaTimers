05f19c496c96ba9f0b2b3112ea852563fb230d02
Andrés F. Alvarez <andresfelipe.alvarez03@gmail.com>
2023-12-07 19:14:27 -0500

    Add select menu basic styles

--- src/components/NewMVP.jsx
+++ src/components/NewMVP.jsx
@@ -5,6 +5,9 @@ import Option from '@mui/joy/Option';
 const NewMVP = ({ time }) => {
     console.log("Hola mundo desde Timear")
 
+    const [open, setOpen] = React.useState(false);
+    const skipRef = React.useRef(false);
+
     const selectStyles = {
       width: '100%',
       borderRadius: 3, 
@@ -15,22 +18,30 @@ const NewMVP = ({ time }) => {
       fontFamily: 'Roboto Flex',
       fontSize: 14,
       fontStyle: 'normal',
-      fontWeight: 700,
-      lineHeight: 'normal'
+      fontWeight: '700 !important',
+      lineHeight: 'normal',
+      boxSizing: 'border-box',
+      transition: 'border 0.3s',
+      border: '1px solid #1d1d1d',
+      ':hover' : {
+        backgroundColor: '#EEEEEE14',
+        border: '1px solid #ededed26',
+        color: '#ABABAB'
+      }
     }
 
-
-    // .select_periodo{
-
-    //   border: 0;
-    //   color: #666666;
-    //   padding-left: 16px;
-    //   padding-right: 12px;
-    //   margin-left: 16px;
-    //   background: url('./images/arrow_down.svg') no-repeat right center;
-    //   background-color: #EEEEEE14;
-    //   padding-right: 20px;
-    // }
+    const optionsStyles = {
+      border: '1px solid #1E1E1E',
+      backgroundColor: '#1E1E1E !important',
+      fontFamily: 'Roboto Flex',
+      fontWeight: 400,
+      color: '#666666',
+      fontSize: 14,
+      ':hover' : {
+      border: '1px solid #ededed26',
+      color: '#ABABAB !important',
+      fontWeight: 500
+    }}
 
   return (
     
@@ -38,34 +49,29 @@ const NewMVP = ({ time }) => {
       <div style={{display: 'flex', flexDirection: 'column', rowGap: 24}} >
         <span className="newMvp_span_mvps">
 
-          {/* <select name="MVP" id="nombre_mvp">
-            <option value="none" selected disabled hidden>Seleccione un MVP</option> 
-            <option value="Garm">Garm</option>
-            <option value="Atroce">Atroce</option>
-            <option value="Maya">Maya</option>
-            <option value="Ungoliant">Ungoliant</option>
-          </select> */}
-
-          {/* <select name="MVP" id="nombre_mvp">
-            <option value="none" selected disabled hidden>Seleccione un Mapa</option> 
-            <option value="mapa1">mapa1</option>
-            <option value="mapa1">mapa1</option>
-          </select> */}
-
-          <Select variant="plain" placeholder="Seleccionar MVP" sx={selectStyles}>
-            <Option value="Atroce">Dog</Option>
-            <Option value="Garm">Cat</Option>
+          <Select variant="plain" placeholder="Seleccionar MVP" sx={selectStyles} listboxOpen={open} onListboxOpenChange={(isOpen) => {
+            if (!skipRef.current) {
+              setOpen(isOpen);
+            }
+          }} >
+            <Option value="Atroce" sx={optionsStyles}>Atroce</Option>
+            <Option value="Garm" sx={optionsStyles}>Garm</Option>
           </Select>
 
           <Select variant="plain" placeholder="Seleccionar Mapa" sx={selectStyles}>
-            <Option value="Ra_fild01">Dog</Option>
-            <Option value="Ve_fild03">Cat</Option>
+            <Option value="Ra_fild01" sx={optionsStyles}>Ra_fild01</Option>
+            <Option value="Ve_fild03" sx={optionsStyles}>Ve_fild03</Option>
           </Select>
 
         </span>
 
         <span className="newMvp_span_hora">
-          <button />
+          <button onClick={() => {
+          skipRef.current = false;
+          setOpen((bool) => !bool);
+        }} onMouseDown={() => {
+          skipRef.current = true;
+        }} />
 
           <input type="number" name="Hora" id="hora_input" placeholder="Hora" />
           <span style={{color: '#ABABAB', fontSize: 28, lineHeight: 0, marginBottom: 7}}>:</span>
