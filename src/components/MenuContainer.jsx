2c726fe4ac7f2ba46c6252ea21156e9f2de0c7b1
Andrés F. Alvarez <andresfelipe.alvarez03@gmail.com>
2024-01-17 19:46:16 -0500

    add basic lateral menu

--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainButton from './menu/MainButton.jsx'
+import Drawer from '@mui/joy/Drawer'
 
 const MenuContainer = () => {
+  const [open, setOpen] = useState(false)
+
+  const toggleDrawer = (inOpen) => (event) => {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return
+    }
+
+    setOpen(inOpen)
+  }
+
   return (
     <>
-      <MainButton />
+      <MainButton toggleDrawer={toggleDrawer(true)} />
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {/* Drawer content */}
+      </Drawer>
     </>
   )
 }
