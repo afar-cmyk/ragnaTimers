a22eb9be0411bf654a50a9c4e8e4fd95157c6e09
Andrés Alvarez <andresfelipe.alvarez03@gmail.com>
2024-05-17 17:01:39 -0500

    add miniboss data

--- src/database/miniBossData.js
+++ src/database/miniBossData.js
@@ -1,4 +1,22 @@
 const miniBoss = {
+  angeling: {
+    fullName: 'Angeling',
+    settings: {
+      thumbnail: {
+        position: '3px 10px',
+        size: '121%'
+      },
+      card: {
+        position: '4px 15px',
+        size: '100%'
+      }
+    },
+    maps: {
+      pay_fild04: { respawn: [60, 90] },
+      xmas_dun01: { respawn: [60, 90] },
+      yuno_fild03: { respawn: [60, 90] }
+    }
+  },
   archangeling: {
     fullName: 'Archangeling',
     settings: {
@@ -13,6 +31,122 @@ const miniBoss = {
     },
     maps: {
       yuno_fild05: { respawn: [60, 63] }
+    }
+  },
+  byorgue: {
+    fullName: 'Byorgue',
+    settings: {
+      thumbnail: {
+        position: '-4px -13px',
+        size: '121%'
+      },
+      card: {
+        position: '16px -40px',
+        size: '100%'
+      }
+    },
+    maps: {
+      thor_v01: { respawn: [60, 60] },
+      thor_v03: { respawn: [30, 30] }
+    }
+  },
+  cato: {
+    fullName: "Cat o' Nine Tails",
+    settings: {
+      thumbnail: {
+        position: '3px 2px',
+        size: '121%'
+      },
+      card: {
+        position: '15px -5px',
+        size: '100%'
+      }
+    },
+    maps: {
+      gld_dun01: { respawn: [20, 20] },
+      pay_dun04: { respawn: [60, 180] }
+    }
+  },
+  chimera: {
+    fullName: 'Chimera',
+    settings: {
+      thumbnail: {
+        position: '-4px -3px',
+        size: '140%'
+      },
+      card: {
+        position: '-9px -35px',
+        size: '113%'
+      }
+    },
+    maps: {
+      gl_cas02: { respawn: [30, 45] }
+    }
+  },
+  darkIllusion: {
+    fullName: 'Dark Illusion',
+    settings: {
+      thumbnail: {
+        position: '-21px -15px',
+        size: '180%'
+      },
+      card: {
+        position: '-2px -42px',
+        size: '134%'
+      }
+    },
+    maps: {
+      gl_church: { respawn: [60, 90] }
+    }
+  },
+  deviling: {
+    fullName: 'Deviling',
+    settings: {
+      thumbnail: {
+        position: '-4px 10px',
+        size: '147%'
+      },
+      card: {
+        position: '-3px 15px',
+        size: '120%'
+      }
+    },
+    maps: {
+      pay_fild04: { respawn: [120, 180] },
+      yuno_fild03: { respawn: [60, 90] }
+    }
+  },
+  dragonFly: {
+    fullName: 'Dragon Fly',
+    settings: {
+      thumbnail: {
+        position: '14px 12px',
+        size: '80%'
+      },
+      card: {
+        position: '44px 7px',
+        size: '80%'
+      }
+    },
+    maps: {
+      moc_fild18: { respawn: [60, 90] }
+    }
+  },
+  eclipse: {
+    fullName: 'Eclipse',
+    settings: {
+      thumbnail: {
+        position: '8px 16px',
+        size: '80%'
+      },
+      card: {
+        position: '28px 14px',
+        size: '80%'
+      }
+    },
+    maps: {
+      prt_fild02: { respawn: [30, 50] },
+      prt_maze03: { respawn: [32, 35] }
     }
   },
   ungoliant: {
