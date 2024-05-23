5d7d6cf3bf4c1afa5983b48ae7fba6cd091496e2
Andrés Alvarez <andresfelipe.alvarez03@gmail.com>
2024-05-22 19:18:48 -0500

    Add server time selector and replace Zustand to Jotai

--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -6,9 +6,17 @@ import DialogTitle from '@mui/joy/DialogTitle'
 import ModalClose from '@mui/joy/ModalClose'
 import FavoritesBar from './favorites/FavoritesBar.jsx'
 import ClockContainer from './clock/ClockContainer.jsx'
+import Select from '@mui/joy/Select'
+import Option from '@mui/joy/Option'
+import { useAtom } from 'jotai'
+import { timeZoneAtom } from '../hooks/stateManager.jsx'
+import ShortUniqueId from 'short-unique-id'
 
 const MenuContainer = () => {
   const [open, setOpen] = useState(false)
+  const [timeZone, setTimeZone] = useAtom(timeZoneAtom)
+
+  const uid = new ShortUniqueId({ length: 5 })
 
   const toggleDrawer = (inOpen) => (event) => {
     if (
@@ -42,6 +50,31 @@ const MenuContainer = () => {
         >
           <ModalClose />
           <DialogTitle>Opciones</DialogTitle>
+          <div>
+            <span className='optionsLabel'>Zona Horaria del servidor:</span>
+            <Select
+              variant='plain'
+              placeholder='Seleccionar Zona Horaria'
+              sx={[selectStyles, Boolean(timeZone) ? selectedStyles : {}]}
+              onChange={(event, value) => {
+                setTimeZone(value)
+              }}
+              value={timeZone || ''}
+              required
+            >
+              {gmtOffsets.map((offset) => {
+                return (
+                  <Option
+                    key={uid.rnd()}
+                    value={offset.value}
+                    sx={optionsStyles}
+                  >
+                    {offset.label}
+                  </Option>
+                )
+              })}
+            </Select>
+          </div>
         </Sheet>
       </Drawer>
     </>
@@ -49,3 +82,82 @@ const MenuContainer = () => {
 }
 
 export default MenuContainer
+
+const selectStyles = {
+  width: '100%',
+  borderRadius: 3,
+  maxHeight: 30,
+  minHeight: 30,
+  color: '#666666',
+  backgroundColor: '#EEEEEE14',
+  fontFamily: 'Roboto Flex',
+  fontSize: 16,
+  fontStyle: 'normal',
+  fontWeight: '700 !important',
+  lineHeight: 'normal',
+  boxSizing: 'border-box',
+  transition: 'border 0.3s',
+  border: '1px solid #1d1d1d',
+  outline: 'none',
+  marginTop: '4px',
+  ':hover': {
+    backgroundColor: '#EEEEEE14',
+    border: '1px solid #ededed26',
+    color: '#ABABAB'
+  },
+  ':focus-visible': {
+    outline: 'none',
+    border: '1px solid #ABABAB !important',
+    color: '#ABABAB'
+  }
+}
+
+const selectedStyles = {
+  backgroundColor: '#EEEEEE14',
+  border: '1px solid #ededed26',
+  color: '#ABABAB'
+}
+
+const optionsStyles = {
+  border: '1px solid #1E1E1E',
+  backgroundColor: '#1E1E1E !important',
+  fontFamily: 'Roboto Flex',
+  fontWeight: 400,
+  color: '#ABABAB',
+  fontSize: 14,
+  ':hover': {
+    border: '1px solid #ededed26',
+    color: '#ABABAB !important',
+    fontWeight: 500
+  }
+}
+
+const gmtOffsets = [
+  { label: 'GMT-12:00 (Baker Island, Howland Island)', value: 'Etc/GMT+12' },
+  { label: 'GMT-11:00 (Niue, Pago Pago)', value: 'Etc/GMT+11' },
+  { label: 'GMT-10:00 (Hawaii)', value: 'Etc/GMT+10' },
+  { label: 'GMT-09:00 (Alaska)', value: 'Etc/GMT+9' },
+  { label: 'GMT-08:00 (Pacific Time, Los Angeles)', value: 'Etc/GMT+8' },
+  { label: 'GMT-07:00 (Mountain Time, Denver)', value: 'Etc/GMT+7' },
+  { label: 'GMT-06:00 (Central Time, Chicago)', value: 'Etc/GMT+6' },
+  { label: 'GMT-05:00 (Eastern Time, New York)', value: 'Etc/GMT+5' },
+  { label: 'GMT-04:00 (Atlantic Time, Chile)', value: 'Etc/GMT+4' },
+  { label: 'GMT-03:00 (Argentina, Buenos Aires)', value: 'Etc/GMT+3' },
+  { label: 'GMT-02:00 (South Georgia/Sandwich Is.)', value: 'Etc/GMT+2' },
+  { label: 'GMT-01:00 (Azores, Cape Verde)', value: 'Etc/GMT+1' },
+  { label: 'GMT+00:00 (UTC, London)', value: 'Etc/GMT' },
+  { label: 'GMT+01:00 (Central European Time, Berlin)', value: 'Etc/GMT-1' },
+  { label: 'GMT+02:00 (Eastern European Time, Cairo)', value: 'Etc/GMT-2' },
+  { label: 'GMT+03:00 (Moscow, Nairobi)', value: 'Etc/GMT-3' },
+  { label: 'GMT+04:00 (Dubai, Samara)', value: 'Etc/GMT-4' },
+  { label: 'GMT+05:00 (Pakistan, Karachi)', value: 'Etc/GMT-5' },
+  { label: 'GMT+06:00 (Bangladesh, Dhaka)', value: 'Etc/GMT-6' },
+  { label: 'GMT+07:00 (Thailand, Bangkok)', value: 'Etc/GMT-7' },
+  { label: 'GMT+08:00 (China, Beijing)', value: 'Etc/GMT-8' },
+  { label: 'GMT+09:00 (Japan, Tokyo)', value: 'Etc/GMT-9' },
+  { label: 'GMT+10:00 (Australia, Sydney)', value: 'Etc/GMT-10' },
+  { label: 'GMT+11:00 (Solomon Islands, New Caledonia)', value: 'Etc/GMT-11' },
+  { label: 'GMT+12:00 (Fiji, New Zealand)', value: 'Etc/GMT-12' },
+  { label: 'GMT+13:00 (Tonga, Apia)', value: 'Etc/GMT-13' },
+  { label: 'GMT+14:00 (Line Islands, Kiritimati)', value: 'Etc/GMT-14' }
+]
--- src/components/clock/ClockContainer.jsx
+++ src/components/clock/ClockContainer.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { format, toZonedTime } from 'date-fns-tz'
+import { useAtom } from 'jotai'
+import { timeZoneAtom } from '../../hooks/stateManager.jsx'
 
 const ClockContainer = () => {
-  return <div className='clockContainer'></div>
+  const [currentTime, setCurrentTime] = useState(new Date())
+  const [timeZone, setTimeZone] = useAtom(timeZoneAtom)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date())
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [])
+
+  useEffect(() => {
+    if (timeZone === null) {
+      const localTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+      setTimeZone(localTimeZone)
+    }
+  }, [timeZone, setTimeZone])
+
+  const zonedTime = toZonedTime(
+    currentTime,
+    timeZone || Intl.DateTimeFormat().resolvedOptions().timeZone
+  )
+  const formattedTime = format(zonedTime, 'hh:mm a')
+
+  console.log(timeZone)
+
+  return (
+    <div className='clockContainer'>
+      <span className='clockTitle'>Hora servidor</span>
+      <span className='clockTime'>
+        <span style={{ marginRight: '3px' }}>{formattedTime.slice(0, 2)}</span>
+        <span className='clockSeparator'>:</span>
+        <span style={{ marginLeft: '3px' }}>{formattedTime.slice(3)}</span>
+      </span>
+    </div>
+  )
 }
 
 export default ClockContainer
