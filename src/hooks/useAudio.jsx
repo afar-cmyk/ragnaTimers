791051aa0b08c1c91c07ad3206b3aa99dd65876d
Afar <andresfelipe.alvarez03@gmail.com>
2025-03-01 20:07:18 -0500

    Get current volume from database

--- src/hooks/useAudio.jsx
+++ src/hooks/useAudio.jsx
@@ -7,16 +7,20 @@ export function useAudio() {
   const playAudio = useCallback(
     async (audioClassOrFile) => {
       let audioPath
+      let isFileName
 
       if (audioClassOrFile === 'respawn' || audioClassOrFile === 'variable') {
+        isFileName = false
         audioPath = await audioContext(
           `./${config[audioClassOrFile + 'File']}.mp3`
         )
       } else {
+        isFileName = true
         audioPath = await audioContext(`./${audioClassOrFile}.mp3`)
       }
 
       let audio = new Audio(audioPath)
+      audio.volume = isFileName ? 1 : config[audioClassOrFile + 'Volume']
       audio.play()
     },
     [config]
